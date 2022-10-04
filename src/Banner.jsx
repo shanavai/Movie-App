d70bb03de05e989e6ea5b3a36924ebe9e4d98563
i <iraklishanava01@gmail.com>
2022-10-04 18:37:16 +0400

    Styled and implemented Homepage using TMDB api

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -1,7 +1,25 @@
-import React from "react";
-import "./Banner.css";
+import axios from "./axios";
+import React, { useEffect, useState } from "react";
+import "./Banner.css"; 
+import requests from "./Requests"
 
 const Banner = () => {
+    const [movie, setMovie] = useState([]);
+
+    useEffect(()=>{
+        async function fetchData(){
+            const request = await axios.get(requests.fetchNetflixOriginals)
+            setMovie(
+                request.data.results[
+                    Math.floor(Math.random() * request.data.results.length - 1)
+                ]
+            )
+            return request;
+        }
+        fetchData();
+    }, [])
+    console.log('movie', movie)
+
   function ellipsis(string, n) {
     return string?.length > n ? string.substr(0, n - 1) + "..." : string;
   }
@@ -10,18 +28,18 @@ const Banner = () => {
       className="banner"
       style={{
         backgroundSize: "cover",
-        backgroundImage: `url("https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Black_flag.svg/1200px-Black_flag.svg.png")`,
+        backgroundImage:  `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
         backgroundPosition: "center center",
       }}
     >
       <div className="banner__contents">
-        <h1 className="banner__title">test</h1>
+        <h1 className="banner__title">{movie?.name || movie?.title || movie?.original_name}</h1>
         <div className="banner__buttons">
           <button className="banner__button">Play</button>
           <button className="banner__button">My List</button>
         </div>
         <h1 className="banner__description">
-            {ellipsis("At a reception for the launch of a new perfume, a man named Frédéric sees a photo of the ruins of an old château by the sea. His mind takes him back to when he was a young boy, walking up to the gate of the château and speaking with a girl who appears to be a little older than he is. Young Frédéric opens the gate and the girl tells him to go to sleep, and later she returns and wakes him. He leaves her and closes the gate behind him. The girl tells him to open the gate but he promises her that he will be back to free her. Frédéric's mind then comes back to reality and he becomes convinced that the meeting between him and the girl actually happened. He tells his mother, who begins to act strangely and tells him he must have imagined it. He does not agree and sets out to find the girl and set her free.", 100)}
+            {ellipsis(movie?.overview, 100)}
         </h1>
       </div>
       <div className="banner--fadeBottom" />
--- src/HomeScreen.jsx
+++ src/HomeScreen.jsx
@@ -1,18 +1,32 @@
-import React from 'react'
-import Banner from './Banner'
-import "./HomeScreen.css"
-import Nav from './Nav'
+import React from "react";
+import Banner from "./Banner";
+import "./HomePage.css";
+import Nav from "./Nav";
+import Row from "./Row";
+import requests from "./Requests";
 
-const HomeScreen = () => {
+const HomePage = () => {
   return (
-    <div className='homeScreen'>
-          {/** Nav */} 
-          <Nav/>
- 
-          <Banner/>
-          {/** Row (list) */}
-    </div>
-  )
-}
+    <div className="homeScreen">
+      {/** Nav */}
+      <Nav />
 
-export default HomeScreen
+      <Banner />
+
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+    </div>
+  );
+};
+
+export default HomePage;
